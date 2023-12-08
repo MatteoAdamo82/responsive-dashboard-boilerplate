d87a90c71d0949d729fa40a6156a8850735a9e24
Matteo <118370682+MatteoAdamo82@users.noreply.github.com>
2023-12-08 20:35:15 +0100

    moved the grid demo data to a separate file

--- js/app.js
+++ js/app.js
@@ -23,36 +23,3 @@ darkMode.addEventListener('click', () => {
   darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
   darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 })
-
-const OrdersDemoData = [
-  {
-    customerName: 'Joe Black',
-    orderNumber: '34902340',
-    paymentAmount: '€ 10,33',
-    status: 'Pending'
-  },
-  {
-    customerName: 'Robert Calling',
-    orderNumber: '12139023',
-    paymentAmount: '€ 1230,00',
-    status: 'Declined'
-  },
-  {
-    customerName: 'Steve Stevens',
-    orderNumber: '23940324909',
-    paymentAmount: '€ 123,20',
-    status: 'Active'
-  },
-]
-
-OrdersDemoData.forEach(order => {
-  const tr = document.createElement('tr');
-  const trContent = `
-        <td>${order.customerName}</td>
-        <td>${order.orderNumber}</td>
-        <td>${order.paymentAmount}</td>
-        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
-    `;
-  tr.innerHTML = trContent;
-  document.querySelector('#recent-orders tbody').appendChild(tr);
-});
